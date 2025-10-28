562ad84453d15c0dd882ee34022d2bec1815d991
Hollow-Dream <isaacperez1117@gmail.com>
2025-10-28 19:31:01 -0400

    Add comments to storage.py for clarity
    Added comments to clarify the purpose of the FILENAME constant and the load_movies and save_movies functions.

--- Main Folder/storage.py
+++ Main Folder/storage.py
@@ -1,14 +1,18 @@
 import json
 import os
 
+#this is the name of the json file that will save all of the movie data
 FILENAME = "movies.json"
 
+#This function load_movies, is used to load data from the movies.json file, so that you can always have your movies.
 def load_movies():
     if os.path.exists(FILENAME):
         with open(FILENAME, "r") as f:
             return json.load(f)
     return []
 
+#This function save_movies is used to save any movies once you add a movie and also once you exit the program.
 def save_movies(movies):
     with open(FILENAME, "w") as f:
         json.dump(movies, f, indent=2)
+
